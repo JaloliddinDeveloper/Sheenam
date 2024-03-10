de3e2c56b653f3f2ae16208e827e90d55ba103fc
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-03-10 18:38:03 +0500

    INFRA:Init Api Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/controller")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+        Ok("Hello Mario");
+    }
+}
